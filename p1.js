ddc581410cc1436d542531c90347c4c2fe58b614
navami16 <navamiprakash16@gmail.com>
2024-03-17 11:07:05 +0530

    index.html

--- p1.js
+++
@@ -1,8 +0,0 @@
-function changeURL(url) {
-    window.location.href = url;
-}
-
-// Triggering the URL change after a delay
-setTimeout(function() {
-    changeURL("http://127.0.0.1:5500/home.html"); // Replace "https://example.com" with your desired URL
-}, 2000);//
